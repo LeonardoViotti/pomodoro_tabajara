a73f810cc3616815de40e694ce4ddbbe029413a4
Leonardo Viotti <lviotti@worldbank.org>
2019-01-14 14:36:15 -0500

    printing count down time!

--- pom.py
+++ pom.py
@@ -27,8 +27,8 @@ sys.stdout.flush()
 
 #### Cycle time
 Wcy_dur = 25 # Work
-Bcy_dur = 5 # Break
-LBcy_dur = 5 # Long break
+Bcy_dur = 7 # Break
+LBcy_dur = 20 # Long break
 
 #### Messages
 Wcy_mes1 = "Work cycle finished! Have a break."
@@ -42,15 +42,29 @@ start_ansN = ["n",  "no", "nao", "nop", "not"]
 
 start_ansYN = start_ansY + start_ansN
 
+#----------------------------------------------------------------------------#
+#### Functions
+#----------------------------------------------------------------------------#
+
+#### Printing on the same line
+def backspace(n):
+    # print((b'\x08').decode(), end='')     # use \x08 char to go back
+    print('\r', end='')                     # use '\r' to go back
+
+def SameLinePrint(p):
+    # print((b'\x08').decode(), end='')     # use \x08 char to go back
+	print(p, end='')
+	backspace(len(str(p)))   
+
 
 #### Subcycle function (in seconds replace with minutes)
 def subcycle(dur, message):
 		t0 = dt.now()
 		ti = dt.now()
 		while ti < t0 + td(minutes=dur):
-		#while ti < t0 + td(seconds=dur):
-
+		#while ti < t0 + td(seconds=dur): # for testing
 			ti = dt.now()
+			SameLinePrint(t0 + td(seconds=dur) - ti)
 		print(message)
 		winsound.Beep(550, 750)
 
