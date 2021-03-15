dec1473b55b90b326b4c44801924838fc0d6ef7f
LeonardoViotti <leonardoviotti@gmail.com>
2021-03-15 14:55:13 -0400

    First basic timer on react.

--- reactjs/src/components/Timer.js
+++ reactjs/src/components/Timer.js
@@ -1,10 +1,36 @@
+import { useTimer } from 'react-timer-hook';
 
-const Timer = () => {
-    return (
-        <div>
-            <h1>[Timer here]</h1>
+
+const Timer = ({ expiryTimestamp }) => {
+    const {
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        resume,
+        restart,
+      } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+    
+      return (
+        <div style={{textAlign: 'center'}}>
+          <div style={{fontSize: '100px'}}>
+            <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+          </div>
+          <p>{isRunning ? 'Running' : 'Not running'}</p>
+          <button onClick={start}>Start</button>
+          <button onClick={pause}>Pause</button>
+          <button onClick={resume}>Resume</button>
+          <button onClick={() => {
+            // Restarts to 5 minutes timer
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 300);
+            restart(time)
+          }}>Restart</button>
         </div>
-    )
+      );
 }
 
 export default Timer
